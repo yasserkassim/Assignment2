2ff0daafc86d76a1adfbadc193c921bd45323988
yasserkassim <kassim.yasser@gmail.com>
2022-04-14 02:55:28 -0400

    Add files via upload

---
+++ serve_html/public/js/test.js
@@ -0,0 +1,14 @@
+
+
+
+// var d = document.getElementsByClassName("content");
+// var dTest = d[0].dataset.testValue;
+// const obj = JSON.parse(dTest);
+// document.write(obj[2].bookName);
+function updateBoard() {
+    alert("Hello");
+}
+
+// //var buttons = document.getElementsByClassName("books");
+var buttons = document.getElementsById("1");
+buttons.addEventListener("click", updateBoard());
