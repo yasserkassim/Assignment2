b139c099cedf63b3b57af87d5f818f0402317ffd
yasserkassim <kassim.yasser@gmail.com>
2022-04-17 00:06:45 -0400

    Add files via upload

---
+++ serve_html/controllers/bookController.js
@@ -0,0 +1,50 @@
+const Books = require("../models/bookModels");
+
+module.exports = {
+    index: (req, res) => {
+        Books.find({}).then(books => {
+            res.render("books/home", {
+                book: books
+            });
+        })
+        .catch(error => {
+            console.log(`Error fetch books: ${error.message}`)
+            res.redirect("/home");
+        });
+    },
+    new: (req, res) => {
+        res.render("books/addBook");
+    },
+    create: (req, res, next) => {
+        let bookParams = {
+            bookName:req.body.bookName,
+            authorName:req.body.authorName,
+            buyLink:req.body.buyLink
+        };
+        Books.create(bookParams).then(books => {
+            res.locals.redirect = "/home";
+            res.locals.books = books;
+            next();
+        })
+        .catch(error => {
+            console.log(`Error saving books: ${error.message}`);
+            next(error);
+        });
+    },
+    redirectView: (req, res, next) => {
+        let redirectPath = res.locals.redirect;
+        if (redirectPath) res.redirect(redirectPath);
+        else next();
+    },
+    delete: (req, res, next) => {
+        let book = req.params.bookName;
+        Books.findOneAndDelete({bookName:book}).then(() => {
+            res.locals.redirect = "/home";
+            next();
+        })
+        .catch(error => {
+            console.log(`Error deleting book by Book Name: ${error.message}`);
+            next();
+        });
+    }
+}
---
+++ serve_html/controllers/homeController.js
@@ -0,0 +1,55 @@
+const Books = require("../models/bookModels");
+
+exports.sendReqParam = (req, res) => {
+    let path = req.url;
+    if (path == '/home'){
+        Books.find( {}, (error, books) => {
+            if (!error) {
+                res.render("home", {data: books});
+            } else {
+                console.log('ERROR' + error);
+            }
+        });
+    }
+    else if (path == '/a2'){
+        res.sendFile(`./public/css/${path}.css`, {
+            root: "./"
+        });
+    }
+    else if (path == '/DeleteABook'){
+        Books.find( {}, (error, books) => {
+            if (!error) {
+                res.render("deleteBook", {data: books});
+            } else {
+                console.log('ERROR' + error);
+            }
+        });
+    }
+    else if (path == '/AddNewBook'){
+        res.render('addBook', {book: new Books()});
+    }
+    else if (path == '/test'){
+        res.sendFile(`./public/js/${path}.js`, {
+            root: "./"
+        });
+    }
+};
+exports.respondWithBook = (req, res) => {
+    let bookNumber = req.params.bookNumber;
+    Books.find( {}, (error, data) => {
+        if (data) res.render(bookNumber, {data: data[bookNumber-1]});
+    });
+};
+
+const httpStatus = require("http-status-codes");
+exports.respondNoResourceFound = (req, res) => {
+    let errorCode = httpStatus.NOT_FOUND;
+    res.status(errorCode);
+    res.send(`${errorCode} | The page does not exist!`);
+};
+exports.respondInternalError = (error, req, res, next) => {
+    let errorCode = httpStatus.INTERNAL_SERVER_ERROR;
+    console.log(`ERROR occurred: ${error.stack}`)
+    res.status(errorCode);
+    res.send(`${errorCode} | Sorry, our application is experiencing a problem!`);
+};
